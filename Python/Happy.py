a707de928d2320ebe48a7339fc44491223b39b76
Saif Hasan <hasan_s@cobra.cs.mercer.edu>
2022-09-13 16:30:24 -0400

    Adding Python

--- Python/Happy.py
+++ Python/Happy.py
@@ -2,43 +2,49 @@
 import math
 
 
-# Function to determine if a number is happy
-def happy(num):
-    m = 0
+def happy(n):
+    """
+    Determines if a number is happy.
+    Reference: "rosettacode.org".
+    
+    :param n: a positive integer
+    :return: a boolean based obn the determination
+    """
     cycle = set()
-    while num != 1:
-        m = 0
-        while num > 0:
-            m += (num % 10) * (num % 10)
-            num = num // 10
-        num = m
-        if num in cycle:
+    while n != 1:
+        n = sum(int(i)**2 for i in str(n))
+        if n in cycle:
             return False
-        else:
-            cycle.add(num)
+        cycle.add(n)
     return True
 
 
-# Function to find the norm of a happy number
-def get_norm(num):
-    norm = num * num
-    m = 0
-    while num != 1:
-        m = 0
-        while num > 0:
-            m += (num % 10) * (num % 10)
-            num = num // 10
-        num = m
-        norm += num * num
+def get_norm(n):
+    """
+    Finds the norm of a happy number.
+    
+    :param n: a happy number
+    :return: the norm
+    """
+    norm = n**2
+    while n != 1:
+        n = sum(int(i)**2 for i in str(n))
+        norm += n**2
     norm = math.sqrt(norm)
     return norm
 
 
-# Function to check input arguments
-def checkArgs(n1, n2):
+def check_args(n1, n2):
+    """
+    Check for validity of the input arguments.
+    If the they are positive and the range is valid, pass them on to another function.
+    
+    :param n1: first integer input given by user
+    :param n2: second integer input given by user
+    """
     # Check if difference between two arguments is zero, also to reject negative integers
     if n2 == n1 or n2 < 0 or n1 < 0:
-        print("Invalid range and/or arguments!\nExiting...")
+        print("\nInvalid range and/or arguments!\nExiting...\n")
         return
     print("\nFirst Argument: " + str(n1))
     print("Second Argument: " + str(n2))
@@ -50,8 +56,13 @@ def checkArgs(n1, n2):
     happy_numbers_in_range(n1, n2)   
         
         
-# Function to find all the happy numbers between the two arguments (inclusive)
 def happy_numbers_in_range(n1, n2):
+    """
+    Finds all the happy numbers between the two arguments (inclusive) and prints in descending order of norms.
+    
+    :param n1: starting number of the range
+    :param n2: ending number of the range
+    """
     # Using a dictionary to store norms and happy numbers as key-value pairs respectively
     mydict = dict()
     for i in range(n1, n2 + 1):
@@ -59,22 +70,23 @@ def happy_numbers_in_range(n1, n2):
             mydict[get_norm(i)] = i
 
     if len(mydict) == 0:
-        print("NOBODY'S HAPPY")
+        print("NOBODY'S HAPPY!\n")
     else:
         # Create a list of sorted keys from the dictionary in descending order
         sorted_vals = list(sorted(mydict, reverse=True))
         # Use the sorted list of keys to print respective values from the dictionary
         for x in sorted_vals[0: 10]:
             print(mydict.get(x))
+        print()
 
 
 def main():
     try:
-      arg1 = int(input("Enter first argument (a whole number): "))
+      arg1 = int(input("\nEnter first argument (a whole number): "))
       arg2 = int(input("Enter second argument (another whole number): "))
-      checkArgs(arg1, arg2)
+      check_args(arg1, arg2)
     except:
-      print("Invalid range and/or arguments!\nExiting...")
+      print("\nInvalid range and/or arguments!\nExiting...\n")
 
 if __name__ == "__main__":
     main()
