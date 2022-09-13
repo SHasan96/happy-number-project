479b2c6e337a93d5367a2462314fefb63deadeb4
Saif Hasan <hasan_s@cobra.cs.mercer.edu>
2022-09-13 00:40:14 -0400

    Adding Java

--- Java/Happy.java
+++ Java/Happy.java
@@ -6,35 +6,40 @@ import java.util.Map.Entry;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-//Happy number project
-
+/**
+  A class that takes finds and prints the happy numbers in a given range.
+  The happy numbers are printed in descending order of their norms and only a maximum of
+  ten numbers in a given range will be printed.
+*/
 public class Happy {
 	
-  public static void main(String[] args){
-    try{
+  public static void main(String[] args) {
+    try {
       Scanner scan = new Scanner(System.in);
-      System.out.print("Enter first argument (a whole number): ");
+      System.out.print("\nEnter first argument (a whole number): ");
       int arg1 = scan.nextInt();
       System.out.print("Enter second argument (another whole number): ");
       int arg2 = scan.nextInt();
       scan.close();
       checkArgs(arg1, arg2);
-    }
-    catch (Exception e){ 
-      System.out.println("Invalid range and/or arguments!\nExiting...");
+    } catch(Exception e) { 
+      System.out.println("\nInvalid range and/or arguments!\nExiting...\n");
     }
   }
 	
-//Function to determine if a number is happy (code taken from "rosetta.org")	
-  public static boolean happy(int number){
+/**
+  Determine if a number is happy. 
+  Reference: "rosetta.org".
+  @param number - is a positive integer.
+  @return a boolean based on the determination.
+*/
+  public static boolean happy(int number) {
     int m = 0;
-    int digit = 0;
     HashSet<Integer> cycle = new HashSet<Integer>();
-    while(number != 1 && cycle.add(number)){
+    while(number != 1 && cycle.add(number)) {
       m = 0;
-      while(number > 0){
-        digit = number % 10;
-        m += digit*digit;
+      while(number > 0) {
+        m += (number % 10)*(number % 10);
         number /= 10;
       }
       number = m;
@@ -42,36 +47,47 @@ public class Happy {
     return number == 1;
   }
   
-//Function to check the arguments
+/**
+  Check the validity of input arguments.
+  Then pass them onto the next function in the correct order.
+  @param n1 - first integer argument.
+  @param n2 - second integer argument.
+*/
   public static void checkArgs(int n1, int n2){
-    //Check if difference between two arguments is zero, also to reject negative integers
+    //Check if difference between two arguments is zero, also to reject negative integers.
     if (n2==n1 || n2<0 || n1<0) {
-      System.out.println("Invalid range and/or arguments!\nExiting...");
+      System.out.println("\nInvalid range and/or arguments!\nExiting...\n");
       return;
     }
     System.out.println("\nFirst Argument: " + n1);
     System.out.println("Second Argument: " +  n2);
-    //Reverse the arguments if necessary to always get a positive difference
+    //Reverse the arguments if necessary to always get a positive difference.
     if (n2<n1) {
       int tmp = n1;
       n1 = n2;
       n2 = tmp;
     }
-    happyNumbersInRange(n2, n1);
+    happyNumbersInRange(n1, n2);
   }      
 
-//Function to find all the happy numbers between the two arguments (inclusive)
-  public static void happyNumbersInRange (int n2, int n1){
-    //This TreeMap will store norm and happy numbers as key-value pairs in descending order of keys
+/** 
+  Find all the happy numbers between the two arguments (inclusive) and print in descending order of norms.
+  @param n1 - starting number of the range.
+  @param n2 - end of the range.
+*/
+  public static void happyNumbersInRange (int n1, int n2) {
+    //This TreeMap will store norm and happy numbers as key-value pairs in descending order of keys.
     Map<Double, Integer> map = new TreeMap<Double, Integer>(Collections.reverseOrder());
 
-    for(int num = n1, count = 0; count<=(n2-n1); count++, num++){
-      if(happy(num)) 
+    for(int num = n1; num<=n2; num++){
+      if(happy(num)) {
         map.put(getNorm(num), num);    
+      }
     }
-    if (map.isEmpty())
-      System.out.println("NOBODY'S HAPPY!");
-    else {
+    if (map.isEmpty()) {
+      System.out.println("NOBODY'S HAPPY!\n");
+    } else {
+      // Use an Iterator to go through the map.
       Iterator<Entry<Double, Integer>> itr = map.entrySet().iterator();
       int i = 1;
       while (itr.hasNext() && i<=10) {
@@ -79,10 +95,15 @@ public class Happy {
         System.out.println(entry.getValue());
         i++;
       }
+      System.out.println();
     }
   }
 
-//Function to find the norm of a happy number
+/**
+  Find the norm of a happy number.
+  @param hnum - a happy number.
+  @return the norm of the happy number passed in.
+*/
   public static double getNorm (int hnum){
     double norm = hnum*hnum;
     int m = 0;
