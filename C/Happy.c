b1e1ffdfb5cd461bdf117ce1873d5d1a1441f879
Saif Hasan <hasan_s@cobra.cs.mercer.edu>
2022-09-18 18:02:29 -0400

    Adding C

--- C/Happy.c
+++ C/Happy.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 #include <math.h>
 
-//Happy Number Project
-
 /*
+    Happy Number Project
+    
     For the finding the happy numbers in a given range and sorting them in descending orders of their respective norms
     we can use structs to store happy numbers and their norms as pairs. These structs need to be stored in an array so
     that we can sort them. Since C has no built-in dynamic list like structures, one solution would be to define our own
@@ -13,14 +13,14 @@
     We will be using the dynamic array for efficiency.
 */
 
-// Using a struct to store store pairs of happy numbers and their respective norms
-// A struct is used since C has no data structure like maps or dictionaries
+// Using a struct to store store pairs of happy numbers and their respective norms.
+// A struct is used since C has no data structure like maps or dictionaries.
 typedef struct {
     double norm;
     int happyNum;
 }NumPair_t;
 
-// This struct is necessary for the dynamic array
+// This struct is necessary for the dynamic array.
 typedef struct{
     NumPair_t *array;
     size_t used;
@@ -29,7 +29,7 @@ typedef struct{
 
 
 //Function prototypes
-int happy(int num);
+int isHappy(int num);
 double getNorm(int num);
 int getInput();
 void checkArgumentsAndProceed(int n1, int n2);
@@ -50,23 +50,34 @@ int main() {
     return 0;
 }
 
-// Get user input and check it
-// This is necessary to handle errors regarding input data type because C does not have exception handling like Java, Python, etc
+/* 
+  Get user input and validate it.
+
+  All data types other than integer will be rejected. 
+  This is necessary to handle errors regarding input data type because C does not have exception handling like Java, Python, etc.
+  Note for self: The behavior of scanf() has issues in some cases. 
+  Reference: "https://stackoverflow.com/questions/26583717/how-to-scanf-only-integer".
+
+  @return an integer argument.
+*/
 int getInput() {
     int n;
-    while(1) {
-        if (scanf("%i", &n) != 1){
-            fprintf(stderr, "\nInvalid range and/or arguments!\nExiting...\n\n");
-            exit(EXIT_FAILURE);
-        }
-        break;
+    float f;
+    if (scanf("%f", &f) != 1 || (int)f != f) {  // This is to reject float values.
+        fprintf(stderr, "\nInvalid range and/or arguments!\nExiting...\n\n");
+        exit(EXIT_FAILURE);
     }
+    n = (int)f; 
     return n;
 }
 
 
-// Determine if a number is happy
-int happy(int num) {
+/*
+  Determines if a number is happy.
+  @param num - a positive integer.
+  @return 1 for ture or 0 for false.
+*/
+int isHappy(int num) {
     int temp = num;
     int sum = 0;
     int rem = 0;
@@ -79,15 +90,20 @@ int happy(int num) {
         }
     temp = sum;
     }
-    if (sum==1)
+    if (sum==1) {
         return 1;
-    else
+    } else {
         return 0;
+    }
 }
 
-// Find and return the norm of a happy number
+/*
+  Find and return the norm of a happy number.
+  @param hn - a happy number.
+  @return norm - norm of the happy number.
+*/  
 double getNorm(int hn) {
-    // Using long variables to deal with large arguments and avoid overflows
+    // Using long variables to deal with large arguments and avoid overflows.
     long hnum = hn;
     long sum = hnum*hnum;
     long m = 0;
@@ -104,7 +120,12 @@ double getNorm(int hn) {
     return norm;
 }
 
-// Check arguments for the range or reverse the parameters if need be
+/*
+  Check arguments for the range or reverse the parameters if need be.
+  Then pass them onto the next function.
+  @param n1 - first argument.
+  @param n2 - second argument.
+*/
 void checkArgumentsAndProceed(int n1, int n2) {
     if (n2==n1 || n2<0 || n1<0) {
         printf("\nInvalid range and/or arguments!\nExiting...\n\n");
@@ -120,14 +141,18 @@ void checkArgumentsAndProceed(int n1, int n2) {
     happyNumbersInRange(n1, n2);
 }
 
-// Find all the happy numbers between the two arguments (inclusive) and print them in descending order of norms
+/*
+  Find all the happy numbers between the two arguments (inclusive).
+  Print them in descending order of norms.
+  @param n1 - start of number range.
+  @param n2 - end of number range.
+*/
 void happyNumbersInRange(int n1, int n2) {
     // An array of structs for each happy number and norm pair
     Array_t a;
     initArray(&a, 10);
-    int i;
-    for (i=n1; i<=n2; i++) {
-        if (happy(i)){
+    for (int i=n1; i<=n2; i++) { 
+        if (isHappy(i)) {
             NumPair_t numPair;
             numPair.norm = getNorm(i);
             numPair.happyNum = i;
@@ -137,43 +162,46 @@ void happyNumbersInRange(int n1, int n2) {
     if (a.used>0) {
         // Sort the array of structs in descending order of norms using qsort() function that takes 4 paramemeters
         qsort(a.array, a.used, sizeof(NumPair_t), compareNorms);
-        int j;
-        for (j=0; j<a.used; j++){
+        for (int j=0; j<a.used; j++){
             printf("%d\n", a.array[j].happyNum);
-            if (j==9)
-                break;  
+            if (j==9) {
+                break; 
+            } 
         }
-        printf("\n");
+    } else {
+        printf("NOBODY'S HAPPY!\n");
     }
-    else {
-        printf("NOBODY'S HAPPY!\n\n");
-    }
+    printf("\n");
     freeArray(&a);
 }
 
-// A comaparator function that will be needed for sorting our array
+/*
+  A comaparator function that will be needed for sorting our array.
+  This will be passed as a paramter for C's qsort() function.
+*/
 int compareNorms(const void *pa, const void *pb) {
     const NumPair_t *p1 = pa;
     const NumPair_t *p2 = pb;
     double diff = p1->norm - p2->norm;
-    // The returned values determine the order of sorting
-    if (diff > 0.0)
+    // The returned values determine the order of sorting (descending in this case).
+    if (diff > 0.0) {
         return -1;
-    else if (diff < 0.0)
+    } else if (diff < 0.0) {
         return 1;
-    else
+    } else {
         return 0;
+    }
 }
 
 /*
-  The following section includes the functions related to our dynamic array
+  The following section includes three functions related to our dynamic array.
   Reference: "https://stackoverflow.com/questions/3536153/c-dynamically-growing-array"
 */
 
-// Initialize the dynamic array
+/*  Initialize the dynamic array. */
 void initArray(Array_t *a, size_t initialSize) {
   a->array = malloc(initialSize * sizeof(NumPair_t));
-  if (a->array == NULL) {
+  if (a->array == NULL) { // Check just in case something goes wrong with memory allocation.
     fprintf(stderr, "Allocation failed!\n\n");
     exit(EXIT_FAILURE);
   }
@@ -181,7 +209,7 @@ void initArray(Array_t *a, size_t initialSize) {
   a->size = initialSize;
 }
 
-// Insert elements into the array
+/* Insert elements into the array. */
 void insertArray(Array_t *a, NumPair_t elem) {
   if (a->used == a->size) {
     a->size *= 2;
@@ -194,7 +222,7 @@ void insertArray(Array_t *a, NumPair_t elem) {
   a->array[a->used++] = elem;
 }
 
-// Deallocate memory used by array
+/* Deallocate memory used by array. */
 void freeArray(Array_t *a) {
   free(a->array);
   a->array = NULL;
